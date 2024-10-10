5b23f00cd0d204c3f9a878c59329cfe5ab6e0cb5
Мария Рудник <mariyrudnik@gmail.com>
2024-10-10 23:19:40 +0300

    new

--- vue3/src/DemoApp.vue
+++ vue3/src/DemoApp.vue
@@ -169,4 +169,67 @@ b { /* used for event dates/times */
   margin: 0 auto;
 }
 
+.fc-toolbar-title {
+  color: #4D4F5C;
+  font-size: 18px !important;
+  font-weight: 400;
+}
+
+.fc-button-group {
+  box-shadow: 0px 2px 3px #0000000D;
+}
+
+.fc-button {
+  background-color: #FFFFFF !important;
+  border: 1px solid #D7DAE2 !important;
+  color: #4D4F5C !important;
+  padding: 8px 16px !important;
+  text-transform: capitalize !important;
+}
+
+.fc-button-active {
+  color: #3B86FF !important;
+}
+
+.fc .fc-button-primary:not(:disabled).fc-button-active:focus, .fc .fc-button-primary:not(:disabled):active:focus {
+  box-shadow: none !important;
+}
+
+.fc-col-header-cell {
+  padding-bottom: 16px !important;
+  padding-top: 16px !important;
+  background: #F5F6FA;
+  border: none !important;
+}
+.fc-col-header {
+  border: 1px solid #EAF0F4;
+  text-transform: uppercase;
+  font-size: 11px !important;
+  color: #A3A6B4 !important;
+}
+.fc-theme-standard .fc-scrollgrid, .fc-theme-standard td, .fc-theme-standard th {
+  border: 1px solid #EAF0F4 !important;
+  border-collapse: collapse;
+}
+
+.fc .fc-daygrid-day-frame {
+  height: 135px;
+  padding-top: 17px;
+  padding-right: 15px;
+  color: #43425D;
+  font-size: 15px;
+}
+.fc .fc-daygrid-day.fc-day-today {
+  background-color: #F5F6FA;
+}
+.fc-h-event {
+  background-color: #3B86FF;
+  color: #FFFFFF;
+}
+.fc-daygrid-event {
+  padding: 7px 0 7px 14px;
+  font-size: 13px;
+  margin-left: 0 !important;
+  margin-right: 0 !important;
+}
 </style>
